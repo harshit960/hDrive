767e58e20090c08c94a60ae87c46f9f4d4b4ae63
harshit960 <raj.harshit960@gmail.com>
2023-07-26 10:18:05 +0530

    upload done

--- old.py
+++ old.py
@@ -2,7 +2,7 @@ import videoConversion as vc
 import fileConversion as fc
 import os
 import requests
-
+import vidUpload as vu
 if __name__ == "__main__":
     os.system("clear")
     while True:
@@ -17,6 +17,7 @@ if __name__ == "__main__":
             print("\nProcessing . .")
             path=fc.fileSave(url)
             [difference,ext,exPath] = vc.binaryToVideo(fc.fileToBinary(path),path)
+            vu.VidUpload(exPath)
             os.system("clear")
             print("Data to Video conversion successfull!\n\n")
         elif option==2:
